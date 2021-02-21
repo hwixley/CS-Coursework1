8a0ecbada77b65b826ae917bbe1f897397820bb9
hwixley <hwixley1@gmail.com>
2021-02-21 17:14:11 +0000

    MITM attack works: buffer file changed successfully

--- mitm/eve.py
+++ mitm/eve.py
@@ -4,7 +4,6 @@ from common import *
 from const import *
 
 args = sys.argv
-
 tags = ["--custom", "--relay", "--break-heart"]
 
 if len(args) != 2 or not (args[1] in tags):
@@ -15,6 +14,8 @@ else:
 
     # Connect to Alice and get her key
     socket1, aes1 = setup("bob", BUFFER_DIR, BUFFER_FILE_NAME)
+
+    # Rename buffer file so each channel has a unique buffer
     buffer1 = BUFFER_FILE_NAME + "1"
     os.rename(BUFFER_DIR + BUFFER_FILE_NAME, BUFFER_DIR + buffer1)
 
@@ -34,18 +35,17 @@ else:
         to_send = NICE_MSG["bob"]
     elif args[1] == tags[2]:
         to_send = BAD_MSG["bob"]
-
     dialog.chat('What Bob actually said: "{}"'.format(to_send))
 
     # Send message to alice
     encrypt_and_send(to_send, aes1, socket1)
     dialog.info('Message sent to Alice! Waiting for reply...')
 
-    # Receive alice's message
+    # Receive alice's response
     received = receive_and_decrypt(aes1, socket1)
     dialog.chat('Alice wanted to say: "{}"'.format(received))
 
-    # Formulate message to send to bob
+    # Formulate response to send to bob
     if args[1] == tags[0]:
         dialog.prompt('Input what you would like Alice to say to Bob:')
         to_send = input()
@@ -53,14 +53,13 @@ else:
         to_send = NICE_MSG["alice"]
     elif args[1] == tags[2]:
         to_send = BAD_MSG["alice"]
-
     dialog.chat('What Alice actually said: "{}"'.format(to_send))
 
-    # Send message to bob
+    # Send response to bob
     encrypt_and_send(to_send, aes2, socket2)
     dialog.info("Message sent to Bob!")
 
-    # Close chat with alice
+    # Close chats
     tear_down(socket1, BUFFER_DIR, buffer1)
     tear_down(socket2, BUFFER_DIR, BUFFER_FILE_NAME)
 
