5ef42e40b2585fde1f2bba66fa8e54ade0a96f97
hwixley <hwixley1@gmail.com>
2021-02-21 16:51:08 +0000

    MITM attack works: still need to change buffer file

--- mitm/eve.py
+++ mitm/eve.py
@@ -21,48 +21,45 @@ else:
     socket2, aes2 = setup("alice", BUFFER_DIR, BUFFER_FILE_NAME)
 
     # Receive bob's message
-    dialog.info("Waiting for message...")
+    dialog.info("Waiting for Bob's message...")
     received = receive_and_decrypt(aes2, socket2)
     dialog.chat('Bob wanted to say: "{}"'.format(received))
 
     # Formulate message to send to alice
-    if args[1] == "--custom":
-        dialog.prompt('Please input message...')
+    if args[1] == tags[0]:
+        dialog.prompt('Input what you would like Bob to say to Alice:')
         to_send = input()
-    elif args[1] == "--relay":
+    elif args[1] == tags[1]:
         to_send = NICE_MSG["bob"]
-    elif args[1] == "--break-heart":
+    elif args[1] == tags[2]:
         to_send = BAD_MSG["bob"]
-    else:
-        print("ERROR: you have an entered an invalid argument")
+
+    dialog.chat('What Bob actually said: "{}"'.format(to_send))
 
     # Send message to alice
     encrypt_and_send(to_send, aes1, socket1)
-    dialog.info('Message sent! Waiting for reply...')
+    dialog.info('Message sent to Alice! Waiting for reply...')
 
     # Receive alice's message
     received = receive_and_decrypt(aes1, socket1)
     dialog.chat('Alice wanted to say: "{}"'.format(received))
 
     # Formulate message to send to bob
-    if args[1] == "--custom":
-        dialog.prompt('Please input message...')
+    if args[1] == tags[0]:
+        dialog.prompt('Input what you would like Alice to say to Bob:')
         to_send = input()
-    elif args[1] == "--relay":
+    elif args[1] == tags[1]:
         to_send = NICE_MSG["alice"]
-    elif args[1] == "--break-heart":
+    elif args[1] == tags[2]:
         to_send = BAD_MSG["alice"]
+
+    dialog.chat('What Alice actually said: "{}"'.format(to_send))
 
     # Send message to bob
     encrypt_and_send(to_send, aes2, socket2)
+    dialog.info("Message sent to Bob!")
 
-    # Close chat
+    # Close chat with alice
     #tear_down(socket1, BUFFER_DIR, BUFFER_FILE_NAME)
-
-
-    #3) Communication cases:
-    #3.1) Relay - decrypt, re-encrypt and send each message using respective keys
-    #3.2) Break Heart - encrypt and send messages (-> Alice: "I hate you!", -> Bob "You broke my heart...")
-    #3.3) Custom - prompt user to add message upon message receival from Bob/Alice, encrypt and send it
 
     exit(0)
